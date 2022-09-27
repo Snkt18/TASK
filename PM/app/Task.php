de081ca64b4a337df0cc9990ab1c2d1f96868fe6
Snkt18 <62741659+Snkt18@users.noreply.github.com>
2022-09-28 01:25:02 +0530

    Update Task.php

--- PM/app/Task.php
+++ PM/app/Task.php
@@ -28,3 +28,126 @@
         ////////////////////////////////////////////////////////////////////////////////
         
         @Query("select f from Flight f where f.source = ?1 and f.destination = ?2 and f.depatureDate = ?3")
+/////////////////////////////////////////////////////////////////////////////////////////
+      flight model
+      export class Flight{
+
+    id?:number;
+    name:string;
+    source:string;
+    destination:string;
+    depatureDate:string;
+    depatureTime:string;
+    arrivalDate:string;
+    arrivalTime:string;
+    duration:number;
+    adultFare:number;
+    childFare:number;
+    seats:number;
+}
+      ////////////////////////////////////////////////////////////////////////////////////
+      
+      import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Flight } from 'src/app/model/flight.model';
+import { FlightServiceService } from 'src/app/service/flight-service.service';
+
+@Component({
+  selector: 'app-book-flight',
+  templateUrl: './book-flight.component.html',
+  styleUrls: ['./book-flight.component.css']
+})
+export class BookFlightComponent implements OnInit {
+  source:string;
+  destination:string;
+  departureDate:string;
+  flightArr:Flight[];
+
+  constructor(private flighService: FlightServiceService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  searchSubmit(searchFlight: NgForm){
+    this.source = searchFlight.value.source;
+    this.destination = searchFlight.value.destination;
+    this.departureDate = searchFlight.value.departureDate;
+    console.log(searchFlight.value.source+" "+searchFlight.value.destination+" "+searchFlight.value.departureDate); 
+    this.flighService.filteredFlight(this.source,this.destination,this.departureDate).subscribe(data=>{
+      this.flightArr = data;
+    });
+  }
+  selectFlight(id: number){
+    console.log(id);
+    localStorage.setItem('id',String(id));
+    this.router.navigateByUrl('/booking-confirmation');
+  }
+}
+      ///////////////////////////////////////////////////////////////////////////////////////
+      
+      import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Flight } from 'src/app/model/flight.model';
+import { FlightServiceService } from 'src/app/service/flight-service.service';
+
+@Component({
+  selector: 'app-book-flight',
+  templateUrl: './book-flight.component.html',
+  styleUrls: ['./book-flight.component.css']
+})
+export class BookFlightComponent implements OnInit {
+  source:string;
+  destination:string;
+  departureDate:string;
+  flightArr:Flight[];
+
+  constructor(private flighService: FlightServiceService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  searchSubmit(searchFlight: NgForm){
+    this.source = searchFlight.value.source;
+    this.destination = searchFlight.value.destination;
+    this.departureDate = searchFlight.value.departureDate;
+    console.log(searchFlight.value.source+" "+searchFlight.value.destination+" "+searchFlight.value.departureDate); 
+    this.flighService.filteredFlight(this.source,this.destination,this.departureDate).subscribe(data=>{
+      this.flightArr = data;
+    });
+  }
+  selectFlight(id: number){
+    console.log(id);
+    localStorage.setItem('id',String(id));
+    this.router.navigateByUrl('/booking-confirmation');
+  }
+}
+      
+////////////////////////////////////////////////////////////////////////////////////////////
+      
+      import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Flight } from '../model/flight.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  postFlight(flight: Flight):Observable<any> {
+    return this.http.post<any>("http://localhost:8714/flight/post",flight);
+  }
+
+  filteredFlight(source: string, destination: string, departureDate: string):Observable<Flight[]> {
+    return this.http.get<Flight[]>("http://localhost:8714/flight/get/"+source+"/"+destination+"/"+departureDate+"");
+  }
+
+  getFlightDetailsById(id: string):Observable<Flight> {
+    return this.http.get<Flight>("http://localhost:8714/flight/one/"+id);
+  }
+
+}
