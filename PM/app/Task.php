89fb0325b895a5d0209310a28e056e71234a02ef
Snkt18 <62741659+Snkt18@users.noreply.github.com>
2022-09-28 00:53:17 +0530

    Update Task.php

--- PM/app/Task.php
+++ PM/app/Task.php
@@ -1,10 +1,30 @@
-<?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Task extends Model
-{
-    //
-}
+  @PostMapping("/flight/post")
+    public Flight postFlight(@RequestBody Flight flight) {
+        return flightRepository.save(flight);
+    }
+    
+    @GetMapping("/flight/get/{source}/{destination}/{date}")
+    public List<Flight> getFilteredFlights(@PathVariable("source") String source,
+                                    @PathVariable("destination") String destination,
+                                    @PathVariable("date") String date) {
+        LocalDate departureDate = LocalDate.parse(date);
+        
+        System.out.println(source+" "+destination+" "+departureDate);
+        
+        return flightRepository.findFilteredFlights(source,destination,departureDate);
+        
+    }
+    @GetMapping("/flight/one/{id}")
+    public Flight getFlightById(@PathVariable("id") Long id) {
+        Optional<Flight> optional = flightRepository.findById(id);
+        
+        if(!optional.isPresent())
+            throw new RuntimeException("Invalid ID");
+        Flight f = optional.get();
+        System.out.println(f);
+        return f;
+    }
+        
+        ////////////////////////////////////////////////////////////////////////////////
+        
+       
